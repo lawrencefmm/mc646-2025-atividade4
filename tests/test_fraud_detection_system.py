bf0e145c36ff89384e1d06e15a4c4046d3ea2321
Gabriel Bonfim Silva de Moraes <78149287+asimov1239@users.noreply.github.com>
2025-10-24 23:04:35 -0300

    update test fraud test

--- tests/test_fraud_detection_system.py
+++ tests/test_fraud_detection_system.py
@@ -10,9 +10,9 @@ def test_check_for_fraud_normal_transaction():
     ## podemos usar datetime para estabelecer datas para os testes
     now = datetime.now()
 
-    transacao_atual = Transaction(amount=500.00, timestamp=now, location="São Paulo")
+    transacao_atual = Transaction(amount=500.00, timestamp=now, location="Sao Paulo")
     transacoes_anterior = [
-        Transaction(amount=150.00,timestamp=now - timedelta(hours=2),location="São Paulo"),
+        Transaction(amount=150.00,timestamp=now - timedelta(hours=2),location="Sao Paulo"),
         Transaction(amount=200.00,timestamp=now - timedelta(days=1),location="Campinas")
     ]
 
@@ -25,25 +25,41 @@ def test_check_for_fraud_normal_transaction():
         blacklisted_locations=blacklisted_locations
     )
 
-    assert not result.is_fraudulent
-    assert not result.is_blocked
-    assert not result.verification_required
+    assert result.is_fraudulent is False
+    assert result.is_blocked is False
+    assert result.verification_required is False
     assert result.risk_score == 0
 
+def test_check_for_fraud_boundary_value_10000():
+    fds = FraudDetectionSystem()
+    now = datetime.now()
+
+    current_transaction = Transaction(amount=10000.00, timestamp=now, location="Sao Paulo")
+    previous_transactions = []
+    blacklisted_locations = []
+
+    result = fds.check_for_fraud(
+        current_transaction=current_transaction,
+        previous_transactions=previous_transactions,
+        blacklisted_locations=blacklisted_locations
+    )
+
+    assert result.risk_score == 0
+    assert not result.is_fraudulent
 
 def test_check_for_fraud_high_transaction_value():
-    ## transação com alto valor
+    ## transacao com alto valor
     fds = FraudDetectionSystem()
 
     now = datetime.now()
 
-    current_transaction = Transaction(amount=15000.00,timestamp=now,location="São Paulo")
+    current_transaction = Transaction(amount=15000.00,timestamp=now,location="Sao Paulo")
 
     previous_transactions = [
         Transaction(
             amount=150.00,
             timestamp=now - timedelta(hours=2),
-            location="São Paulo"
+            location="Sao Paulo"
         )
     ]
 
@@ -67,7 +83,7 @@ def test_check_for_fraud_excessive_transactions_in_short_period():
 
     now = datetime.now()
 
-    current_transaction = Transaction(amount=50.00,timestamp=now,location="São Paulo")
+    current_transaction = Transaction(amount=50.00,timestamp=now,location="Sao Paulo")
 
     previous_transactions = [
         Transaction(
@@ -91,6 +107,145 @@ def test_check_for_fraud_excessive_transactions_in_short_period():
     assert result.risk_score == 30
 
 
+def test_check_for_fraud_excessive_transactions_in_short_period_60_minutes():
+    ## muitas transacoes em pouco tempo
+    fds = FraudDetectionSystem()
+
+    now = datetime.now()
+
+    current_transaction = Transaction(amount=50.00,timestamp=now,location="Sao Paulo")
+
+    previous_transactions = [
+        Transaction(
+            amount=25.00,
+            timestamp=now - timedelta(minutes=60),
+            location="Campinas"
+        ) for i in range(1, 12)
+    ]
+
+    blacklisted_locations = ["Caracas", "Pyongyang"]
+
+    result = fds.check_for_fraud(
+        current_transaction=current_transaction,
+        previous_transactions=previous_transactions,
+        blacklisted_locations=blacklisted_locations
+    )
+
+    assert not result.is_fraudulent
+    assert result.is_blocked
+    assert not result.verification_required
+    assert result.risk_score == 30
+
+
+def test_check_for_fraud_excessive_transactions_in_short_period_over_60_minutes():
+    ## muitas transacoes em pouco tempo
+    fds = FraudDetectionSystem()
+
+    now = datetime.now()
+
+    current_transaction = Transaction(amount=50.00,timestamp=now,location="Sao Paulo")
+
+    previous_transactions = [
+        Transaction(
+            amount=25.00,
+            timestamp=now - timedelta(seconds=3630),
+            location="Campinas"
+        ) for i in range(1, 12)
+    ]
+
+    blacklisted_locations = ["Caracas", "Pyongyang"]
+
+    result = fds.check_for_fraud(
+        current_transaction=current_transaction,
+        previous_transactions=previous_transactions,
+        blacklisted_locations=blacklisted_locations
+    )
+
+    assert not result.is_fraudulent
+    assert not result.is_blocked
+    assert not result.verification_required
+    assert result.risk_score == 0
+
+
+def test_check_for_fraud_high_value_and_excessive_transactions():
+    # teste para matar o mutante 97
+    fds = FraudDetectionSystem()
+    now = datetime.now()
+
+    current_transaction = Transaction(amount=12000.00, timestamp=now, location="Sao Paulo")
+
+    previous_transactions = [
+        Transaction(
+            amount=25.00,
+            timestamp=now - timedelta(minutes=i * 5),
+            location="Campinas"
+        ) for i in range(1, 12)
+    ]
+
+    blacklisted_locations = ["Caracas"]
+
+    result = fds.check_for_fraud(
+        current_transaction=current_transaction,
+        previous_transactions=previous_transactions,
+        blacklisted_locations=blacklisted_locations
+    )
+
+    assert result.risk_score == 80
+    assert result.is_blocked
+    assert result.is_fraudulent
+
+def test_check_for_fraud_location_change_boundary_30_min():
+    fds = FraudDetectionSystem()
+    now = datetime.now()
+
+    current_transaction = Transaction(amount=200.00, timestamp=now, location="Rio de Janeiro")
+
+    previous_transactions = [
+        Transaction(amount=150.00, timestamp=now - timedelta(seconds=1806), location="Sao Paulo")
+    ]
+
+    blacklisted_locations = ["Caracas"]
+
+    result = fds.check_for_fraud(
+        current_transaction=current_transaction,
+        previous_transactions=previous_transactions,
+        blacklisted_locations=blacklisted_locations
+    )
+
+    assert result.risk_score == 0
+    assert not result.is_fraudulent
+
+def test_check_for_fraud_excessive_transactions_in_short_period_10_transactions():
+    ## muitas transacoes em pouco tempo
+    fds = FraudDetectionSystem()
+
+    now = datetime.now()
+
+    current_transaction = Transaction(amount=50.00,timestamp=now,location="Sao Paulo")
+
+    previous_transactions = [
+        Transaction(
+            amount=25.00,
+            timestamp=now - timedelta(minutes=i * 5),
+            location="Campinas"
+        ) for i in range(1, 11)
+    ]
+
+    blacklisted_locations = ["Caracas", "Pyongyang"]
+
+    result = fds.check_for_fraud(
+        current_transaction=current_transaction,
+        previous_transactions=previous_transactions,
+        blacklisted_locations=blacklisted_locations
+    )
+
+    assert not result.is_fraudulent
+    assert not result.is_blocked
+    assert not result.verification_required
+    assert result.risk_score == 0
+
+
+
 def test_check_for_fraud_rapid_location_change():
     fds = FraudDetectionSystem()
 
@@ -99,7 +254,7 @@ def test_check_for_fraud_rapid_location_change():
     current_transaction = Transaction(amount=200.00, timestamp=now, location="Rio de Janeiro")
 
     previous_transactions = [
-        Transaction(amount=150.00,timestamp=now - timedelta(minutes=15),location="São Paulo")
+        Transaction(amount=150.00,timestamp=now - timedelta(minutes=15),location="Sao Paulo")
     ]
 
     blacklisted_locations = ["Caracas", "Pyongyang"]
@@ -123,7 +278,7 @@ def test_check_for_fraud_location_in_blacklist():
     current_transaction = Transaction(amount=500.00, timestamp=now, location="Pyongyang")
 
     previous_transactions = [
-        Transaction(amount=100.00, timestamp=now - timedelta(hours=2), location="São Paulo")
+        Transaction(amount=100.00, timestamp=now - timedelta(hours=2), location="Sao Paulo")
     ]
 
     blacklisted_locations = ["Caracas", "Pyongyang", "Tehran"]
@@ -169,7 +324,7 @@ def test_check_for_fraud_multiple_triggers_high_value_and_location_change():
     current_transaction = Transaction(amount=12000.00, timestamp=now, location="Rio de Janeiro")
 
     previous_transactions = [
-        Transaction(amount=100.00, timestamp=now - timedelta(minutes=20), location="São Paulo")
+        Transaction(amount=100.00, timestamp=now - timedelta(minutes=20), location="Sao Paulo")
     ]
 
     blacklisted_locations = ["Caracas"]
@@ -184,3 +339,25 @@ def test_check_for_fraud_multiple_triggers_high_value_and_location_change():
     assert not result.is_blocked
     assert result.verification_required
     assert result.risk_score == 70
+
+
+def test_check_for_fraud_location_change_exact_30_min():
+    fds = FraudDetectionSystem()
+    now = datetime.now()
+
+    current_transaction = Transaction(amount=200.00, timestamp=now, location="Rio de Janeiro")
+
+    previous_transactions = [
+        Transaction(amount=150.00, timestamp=now - timedelta(minutes=30), location="Sao Paulo")
+    ]
+
+    blacklisted_locations = ["Caracas"]
+
+    result = fds.check_for_fraud(
+        current_transaction=current_transaction,
+        previous_transactions=previous_transactions,
+        blacklisted_locations=blacklisted_locations
+    )
+
+    assert result.risk_score == 0
+    assert not result.is_fraudulent
